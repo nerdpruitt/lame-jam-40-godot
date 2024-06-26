52dc066944e767ae0588d8232ba2d16f67fb1549
Nick Pruitt <pruitt.nick@gmail.com>
2024-06-25 22:12:05 -0500

    Parallax Backgrounds

--- scripts/PlayerCamera.gd
+++ scripts/PlayerCamera.gd
@@ -7,6 +7,8 @@ func _ready():
 	var tileSize = tilemap.tile_set.tile_size
 	var worldSizeInPixels = mapRect.size * tileSize
 	
+	print(worldSizeInPixels)
+	
 	limit_right = worldSizeInPixels.x
 	limit_bottom = worldSizeInPixels.y
 	limit_left = 0
