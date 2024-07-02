dc82f817666bf6243cff93d5ff99368a9c7fc7be
Nick Pruitt <pruitt.nick@gmail.com>
2024-07-01 21:32:22 -0500

    Player health bar

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,10 +4,28 @@ extends CharacterBody2D
 @export var acceleration = 1500
 @export var friction = 800
 
+signal health_depleted
+
+var health = 100.0
+
 var input = Vector2.ZERO
 
 func _physics_process(delta):
 	player_movement(delta)
+	detect_mob_overlap(delta)
+
+func detect_mob_overlap(delta):
+	const DAMAGE_RATE = 50.0
+	
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		
+		
+	if health <= 0.0:
+		health_depleted.emit()
 
 func get_input():
 	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
